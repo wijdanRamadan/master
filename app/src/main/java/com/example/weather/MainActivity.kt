da1edf00035eaa5bf3102b007a5c05f103762e4e
wijdan <wijdanramadan1993@gmail.com>
2019-07-09 23:04:48 +0300

    first commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_holder, myFragment(), "weather")
+                .commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weather/myFragment.kt
@@ -0,0 +1,45 @@
+package com.example.weather
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class myFragment : Fragment()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment , container , false)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
